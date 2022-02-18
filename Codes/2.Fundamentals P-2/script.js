59bd4ede178a0ac52a7cb7c895769d5a697d7d5a
saini-sapna-au26 <sapna98saini@gmail.com>
2022-02-18 20:30:22 +0530

    js

--- Codes/2.Fundamentals P-2/script.js
+++ Codes/2.Fundamentals P-2/script.js
@@ -63,15 +63,49 @@
 
 //5. Function calling other functions
 
-function cutFruits(fruit) {
-  return fruit * 3;
-}
+// function cutFruits(fruit) {
+//   return fruit * 3;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruits(apples);
-  const orangesPieces = cutFruits(oranges);
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruits(apples);
+//   const orangesPieces = cutFruits(oranges);
 
-  const juice = `juice with ${applePieces} piece of apple and ${orangesPieces} piece of oranges.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
+//   const juice = `juice with ${applePieces} piece of apple and ${orangesPieces} piece of oranges.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+// Functions ==>
+const calAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+// const yearOfRetirement = function (birthYear, name) {
+//   const age = calAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     return retirement;
+//     console.log(`${name} retires in ${retirement} year`);
+//   } else {
+//     return -1;
+//     console.log(`${name} has already retired 🎉`);
+//   }
+// };   //In this case only return statements showing their result because console.log is after the return statements
+
+const yearOfRetirement = function (birthYear, name) {
+  const age = calAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${name} retires in ${retirement} year`);
+    return retirement;
+  } else {
+    console.log(`${name} has already retired 🎉`);
+    return -1;
+  }
+}; //In this case both return and console statements show their result
+
+console.log(yearOfRetirement(1999, "sapna"));
+console.log(yearOfRetirement(1950, "mike"));
