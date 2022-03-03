16cbf81b589c44a259ce8b11cc22cf3deabb7c7a
saini-sapna-au26 <sapna98saini@gmail.com>
2022-03-04 01:09:13 +0530

    project

---
+++ Projects/02.Modal Window/script.js
@@ -0,0 +1,39 @@
+"use script";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+// console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener("click", function () {
+//     console.log("Button clicked");
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//   });
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
