be9476a869153c1b1ec9f1ff03f55b1a7d1195b1
saini-sapna-au26 <sapna98saini@gmail.com>
2022-03-09 18:48:24 +0530

    js

--- Codes/07.Data structure, Modern operators/index.js
+++ Codes/07.Data structure, Modern operators/index.js
@@ -56,6 +56,7 @@ console.log(p, q, r);
 //////////////////////////////////////////
 
 // Destructing objects
+/*
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
@@ -131,3 +132,86 @@ const {
   fri: { open: o, close: c },
 } = openingHourse;
 console.log(o, c);
+*/
+/////////////////////////////////////////////////////////////////
+
+//Spread operator(.....)
+const arr = [7, 8, 9];
+const addNewArr = [1, 2, arr[0], arr[1], arr[2]]; // add in the begining
+console.log(addNewArr);
+
+const newArr = [1, 2, ...arr]; // add with spread opeartors
+console.log(newArr);
+
+console.log(...newArr); // it gave individual array i.e 1,2,3,4,5
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizaa", "Pasta", "Risotto"],
+  openingHourse: {
+    thu: { open: 12, close: 22 },
+    fri: { open: 11, close: 23 },
+    sat: { open: 0, close: 24 }, //open 24 hrs
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+};
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// join 2 array
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, string, maps, set, not object
+const str = "sapna";
+const letters = [...str, " ", "R."];
+console.log(letters);
+console.log(...str);
+// we can only use spread operators in arrays or when er pass values in functions
+// console.log(`${...str} saini`) // we can't use spread init becz of multiples values
+
+// Real-world example
+const ingredients = [
+  // prompt("Let's make paste! ingredients 1? "),
+  // prompt("Ingredients 2?"),
+  // prompt("indgredient 3?"),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// both r same
+restaurant.orderPasta(...ingredients);
+
+// objects
+const newRestaurant = { founderIn: 1998, ...restaurant, founder: "Guiseppe" };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
